cd1e3791c3e79da894d00e5179f7147c579f3b7d
Ashish6745 <ashupawar9917@gmail.com>
2025-09-29 21:23:28 +0530

    password hashing, access token and refresh token generating hook methods added

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,6 +1,6 @@
 import mongoose, {Schema} from "mongoose";
-
-
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 const userSchema  = new Schema({
     avatar :{
         type: {
@@ -66,6 +66,54 @@ const userSchema  = new Schema({
 
 
     },{timestamps:true});
+
+
+  // mongoose pre-hook to decrypt the password before saving to the database ...
+  userSchema.pre("save", async function(next){
+   
+    if(!this.isModified("password")){
+      return next();
+    }
+   
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+  });
+
+
+  // verifying if the password is correct or not
+  userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  };
+
+// generating Access Token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+
+// generating Refresh Token
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+
+
+
 
 
 

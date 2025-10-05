b0276d2201396eff8ba638c42ea11fe840b90189
Ashish6745 <ashupawar9917@gmail.com>
2025-10-05 20:50:54 +0530

    User Logout added succesfully

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 const router = Router();
-import {loginUser, registerUser} from '../controllers/auth.controllers.js';
+import {loginUser, logoutUser, registerUser} from '../controllers/auth.controllers.js';
 import  {userRegisterValidator, loginValidator} from '../validators/validators.js'
 import {validate} from '../middlewares/validator.middlewares.js'
-
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 
 router.route('/register').post(userRegisterValidator(), validate,registerUser);
 router.route('/login').post(loginValidator(), validate, loginUser);
+// secured routes
+router.route('/logout').post(verifyJWT, logoutUser);
 
 
 export default router;
